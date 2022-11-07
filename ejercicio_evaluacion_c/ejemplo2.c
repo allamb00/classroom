21436d954184364c43c929b6b426d32937d4b342
allamb00 <allamb00@estudiantes.unileon.es>
2022-11-07 10:02:38 +0100

    Se han realizado los ejericios 2 y 3

--- ejercicio_evaluacion_c/ejemplo2.c
+++ ejercicio_evaluacion_c/ejemplo2.c
@@ -1,16 +1,21 @@
 #include <stdlib.h>
 
-/*  DCL31-C  */
+/*  DCL38-C  */
 
 struct flexArrayStruct {
 	int num;
-	int data[1];
+	/*  Se usa un array flexible para conseguir un struct de tamaño dinámico, 
+	 *  aunque enla ISO 90 no están soportados.
+	 */
+	int data[];
 };
 
 void func(size_t array_size) {
+	/*  Declaración de variables fuera del bucle y del código  */
+	size_t i;
 	/*  Space is allocated for the struct  */
 	struct flexArrayStruct *structP = (struct flexArrayStruct *)
-		malloc(sizeof(struct flexArrayStruct) + sizeof(int) * (array_size - 1));
+		malloc(sizeof(struct flexArrayStruct) + sizeof(int) * array_size);
 	if(structP == NULL) {
 		/*  Handle malloc failure  */
 	}
@@ -21,7 +26,7 @@ void func(size_t array_size) {
 	 *  Access data[] as if it had been allocated
 	 *  as data[array_size].
 	 */
-	for(size_t i = 0; i < array_size; ++i) {
+	for(i = 0; i < array_size; ++i) {
 		structP -> data[i] = 1;
 	}
 }
--- ejercicio_evaluacion_c/ejemplo3.c
+++ ejercicio_evaluacion_c/ejemplo3.c
@@ -5,9 +5,11 @@
 extern void f(int i);
 
 void func(int expr) {
+	/*  Se saca el código que no está en los case fuera del switch  */
+	int i = 4;
+	f(i);
+
 	switch (expr) {
-		int i = 4;
-		f(i);
 		case 0:
 			i = 17;
 		default:
